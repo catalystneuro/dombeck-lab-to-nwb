1b564382b9aad103d65ebe42987ab6150b49c3af
weiglszonja <wszonja@gmail.com>
2024-04-18 15:12:21 +0200

    update interface

--- src/dombeck_lab_to_nwb/nagappan_embargo_2023/interfaces/nagappan_embargo_2023_motioncorrectioninterface.py
+++ src/dombeck_lab_to_nwb/nagappan_embargo_2023/interfaces/nagappan_embargo_2023_motioncorrectioninterface.py
@@ -1,28 +1,25 @@
 from pathlib import Path
 from typing import Union
 
-from neuroconv.datainterfaces.ophys.baseimagingextractorinterface import BaseImagingExtractorInterface
-from neuroconv.utils import DeepDict
+from neuroconv import BaseDataInterface
+from neuroconv.tools.roiextractors.imagingextractordatachunkiterator import ImagingExtractorDataChunkIterator
 from pynwb import NWBFile
+from roiextractors import TiffImagingExtractor
 
 from dombeck_lab_to_nwb.nagappan_embargo_2023.utils import get_xy_shifts, add_motion_correction
 
 
-class NagappanEmbargo2023MotionCorrectionInterface(BaseImagingExtractorInterface):
+class NagappanEmbargo2023MotionCorrectionInterface(BaseDataInterface):
     """Data interface for adding the motion corrected image data."""
 
     display_name = "NagappanEmbargo2023MotionCorrection"
     associated_suffixes = (".tif", ".mat")
     info = "Custom interface for motion corrected imaging data."
 
-    ExtractorName = "TiffImagingExtractor"
-
     def __init__(
         self,
         file_path: Union[str, Path],
         xy_shifts_file_path: Union[str, Path],
-        sampling_frequency: float,
-        photon_series_name: str = "TwoPhotonSeriesCorrected",
         verbose: bool = True,
     ):
         """
@@ -34,46 +31,33 @@ class NagappanEmbargo2023MotionCorrectionInterface(BaseImagingExtractorInterface
             The path to the motion corrected imaging data (.tif file).
         xy_shifts_file_path : Union[str, Path]
             The path to the xy shifts file (.mat file).
-        sampling_frequency : float
-            The sampling frequency of the imaging data.
-        photon_series_name : str, optional
-            The name of the corrected photon series to add, by default "TwoPhotonSeriesCorrected".
         """
-        self.xy_shifts_file_path = xy_shifts_file_path
-        self.photon_series_name = photon_series_name
-        super().__init__(file_path=file_path, sampling_frequency=sampling_frequency, verbose=verbose)
-
-    def get_metadata(self) -> DeepDict:
-
-        metadata = super().get_metadata()
-        two_photon_series_metadata = metadata["Ophys"]["TwoPhotonSeries"][0]
-
-        two_photon_series_metadata.update(
-            name=f"{self.photon_series_name}", description="Motion corrected two-photon imaging data."
-        )
-
-        return metadata
+        super().__init__(file_path=file_path, xy_shifts_file_path=xy_shifts_file_path, verbose=verbose)
 
     def add_to_nwbfile(
         self,
         nwbfile: NWBFile,
         metadata: dict,
-        photon_series_index: int = 0,
+        sampling_frequency: float = None,
+        corrected_image_stack_name: str = "CorrectedImageStack",
         stub_test: bool = False,
     ) -> None:
 
-        super().add_to_nwbfile(
-            nwbfile=nwbfile,
-            metadata=metadata,
-            stub_test=stub_test,
-            parent_container="processing/ophys",
-            photon_series_index=photon_series_index,
+        xy_shifts = get_xy_shifts(self.source_data["xy_shifts_file_path"])
+        # Sampling frequency is required for the extractor, but we will write with the timestamps from the original data
+        imaging_extractor = TiffImagingExtractor(
+            file_path=self.source_data["file_path"], sampling_frequency=sampling_frequency
         )
-
-        xy_translation = get_xy_shifts(self.xy_shifts_file_path)
-        raw_two_photon_series_name = self.photon_series_name.replace("Corrected", "")
+        corrected = (
+            imaging_extractor.get_video(end_frame=100)
+            if stub_test
+            else ImagingExtractorDataChunkIterator(
+                imaging_extractor=imaging_extractor,
+            )
+        )
         add_motion_correction(
             nwbfile=nwbfile,
-            xy_shifts=xy_translation,
-            original_two_photon_series_name=raw_two_photon_series_name,
+            corrected=corrected,
+            xy_shifts=xy_shifts[:100, :] if stub_test else xy_shifts,
+            corrected_image_stack_name=corrected_image_stack_name,
         )
