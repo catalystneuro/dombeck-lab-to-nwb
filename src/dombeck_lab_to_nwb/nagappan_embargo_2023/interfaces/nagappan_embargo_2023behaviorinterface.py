acd25ee0dc94fa584c6c5c59386d66f25f3cacac
weiglszonja <wszonja@gmail.com>
2024-05-02 12:39:43 +0200

    add time alignment

--- src/dombeck_lab_to_nwb/nagappan_embargo_2023/interfaces/nagappan_embargo_2023behaviorinterface.py
+++ src/dombeck_lab_to_nwb/nagappan_embargo_2023/interfaces/nagappan_embargo_2023behaviorinterface.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 from neuroconv import BaseTemporalAlignmentInterface
 from neuroconv.tools import get_module
+from neuroconv.tools.signal_processing import get_rising_frames_from_ttl
 from neuroconv.utils import FilePathType
 from pynwb import NWBFile, TimeSeries
 from pynwb.epoch import TimeIntervals
@@ -20,6 +21,7 @@ class NagappanEmbargoBehaviorInterface(BaseTemporalAlignmentInterface):
     def __init__(
         self,
         dat_file_path: FilePathType,
+        daq_dat_file_path: FilePathType,
         mat_file_path: FilePathType,
         verbose: bool = True,
     ):
@@ -30,6 +32,8 @@ class NagappanEmbargoBehaviorInterface(BaseTemporalAlignmentInterface):
         ----------
         dat_file_path : FilePathType
             The path to the .dat file.
+        daq_dat_file_path : FilePathType
+            The path to the .dat file containing the DAQ data.
         mat_file_path: FilePathType
             The path to the .mat file.
         verbose: bool, default: True
@@ -42,8 +46,16 @@ class NagappanEmbargoBehaviorInterface(BaseTemporalAlignmentInterface):
         mat_file_path = Path(mat_file_path)
         if ".mat" not in mat_file_path.suffixes:
             raise IOError(f"The file '{mat_file_path.stem}' is not a .mat file.")
+        if ".dat" not in daq_dat_file_path.suffixes:
+            raise IOError(f"The file '{daq_dat_file_path.stem}' is not a .dat file.")
 
-        super().__init__(file_path=file_path, mat_file_path=mat_file_path, verbose=verbose)
+        super().__init__(
+            file_path=file_path, daq_file_path=daq_dat_file_path, mat_file_path=mat_file_path, verbose=verbose
+        )
+
+        self._vars = self._read_mat_data()
+        self._sampling_frequency = self._vars["daq"]["sampleRate"]
+        self._num_daq_channels = self._vars["si"]["licks"]
 
     def _read_mat_data(self):
         """Read the data from the .mat file."""
@@ -70,6 +82,59 @@ class NagappanEmbargoBehaviorInterface(BaseTemporalAlignmentInterface):
         num_frames = data.shape[0]
         data = data.reshape((num_frames // num_inputs, num_inputs))
         return data
+
+    def _read_daq_data(self):
+        with open(self.source_data["daq_file_path"], "rb") as f:
+            file_data = f.read()
+
+        # Initialize empty array to hold individual bits
+        bits = np.zeros(len(file_data) * 8, dtype=np.uint8)
+
+        # Loop over each byte
+        for i, byte in enumerate(file_data):
+            # Loop over each bit in the byte
+            for j in range(8):
+                # Extract bit from byte
+                bit = (byte >> j) & 1
+                # Store bit in the array
+                bits[i * 8 + j] = bit
+
+        num_bits = bits.shape[0]
+        data_bits = bits[: num_bits - num_bits % self._num_daq_channels]
+        num_bits = data_bits.shape[0]
+        data_bits = data_bits.reshape((num_bits // self._num_daq_channels, self._num_daq_channels))
+        return data_bits
+
+    def get_aligned_times_from_ttl(self) -> [np.ndarray, np.ndarray]:
+        """Returns the aligned imaging and camera times based on the TTL pulses."""
+
+        events_data = self._read_data()
+        # time elapsed per loop based on CPU clock (logged every ~30ms)
+        times_from_cpu_clock = events_data[:, 0]
+        # time elapsed per loop based on DAQ clock (logged every ~30ms)
+        times_from_daq_clock = np.cumsum(events_data[:, 1]) / self._sampling_frequency
+
+        daq_data = self._read_daq_data()
+        num_daq_samples = daq_data[:, 0].shape[0]
+        times_from_daq = (np.arange(1, num_daq_samples + 1) / self._sampling_frequency) - (
+            times_from_daq_clock[0] - times_from_cpu_clock[0]
+        )
+
+        rising_frames_from_imaging = np.where((daq_data[1:, 1] > 0.8) & (daq_data[:-1, 1] < 0.1))[0]
+        rising_frames_from_imaging = rising_frames_from_imaging + 1  # first high point of imaging pulse
+
+        # get frame times
+        frame_times = times_from_daq[rising_frames_from_imaging]  # time of first point of the high pulse
+        # correct frame_times to center of imaging frame rather than the beginning
+        dt = np.mean(np.diff(frame_times))
+        imaging_times = frame_times + dt / 2
+
+        rising_frames_from_camera = get_rising_frames_from_ttl(
+            trace=daq_data[:, 2],
+        )
+        camera_times = times_from_daq[rising_frames_from_camera]
+
+        return imaging_times, camera_times
 
     def get_original_timestamps(self) -> np.ndarray:
         return self._read_data()[:, 0]
@@ -119,8 +184,8 @@ class NagappanEmbargoBehaviorInterface(BaseTemporalAlignmentInterface):
 
             for start_index, end_index in zip(starts, ends):
                 intervals.add_interval(
-                    start=timestamps[start_index],
-                    end=timestamps[end_index],
+                    start_time=timestamps[start_index],
+                    stop_time=timestamps[end_index],
                     brake_value=brake_value,
                 )
 
