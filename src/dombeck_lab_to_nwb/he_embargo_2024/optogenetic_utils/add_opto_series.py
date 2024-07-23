3a218c8f5aa3cfb114d9e957af900cc317e45a97
weiglszonja <wszonja@gmail.com>
2024-07-23 12:17:24 +0200

    add utility for extracting opto stimulus parameters

--- src/dombeck_lab_to_nwb/he_embargo_2024/optogenetic_utils/add_opto_series.py
+++ src/dombeck_lab_to_nwb/he_embargo_2024/optogenetic_utils/add_opto_series.py
@@ -1,7 +1,55 @@
 import numpy as np
 from neuroconv.tools.roiextractors import add_devices
+from pymatreader import read_mat
 from pynwb import NWBFile
 from pynwb.ogen import OptogeneticSeries
+
+
+def get_stimulation_parameters_from_mat_file(mat_file_path: str, session_id: str):
+    """
+    Returns a dictionary with the stimulation parameters extracted from the .mat file.
+    e.g. {'power_in_W': 0.4, 'duration_in_s': 5.0, 'frequency_in_Hz': 6.0, 'pulse_width_in_s': 0.008}
+
+    Parameters
+    ----------
+    mat_file_path : str
+        The path to the .mat file containing the stimulation parameters.
+    session_id : str
+        The identifier of the session.
+    """
+    stimulation_parameters_mat = read_mat(mat_file_path)
+    values = stimulation_parameters_mat["#subsystem#"]["MCOS"][2]
+    column_names = stimulation_parameters_mat["#subsystem#"]["MCOS"][7]
+    filenames = values[column_names.index("filename")]
+    session_ids = [filename.split(".")[0] for filename in filenames]
+    assert session_id in session_ids, f"Expected session_id '{session_id}', found {session_ids}."
+    session_index = session_ids.index(session_id)
+    power_stim_index = column_names.index("power_stim")
+    power_in_mW = values[power_stim_index][session_index]
+
+    length_stim_index = column_names.index("length_stim")
+    duration_in_ms = values[length_stim_index][session_index]
+
+    has_opto = power_in_mW != 0
+
+    if not has_opto:
+        return None
+
+    type_stim_index = column_names.index("type_stim")
+    type_stim = values[type_stim_index][session_index]
+    frequency_str, pulse_width_str = type_stim.split("-")
+    frequency_in_Hz = float(frequency_str.replace("Hz", ""))
+    pulse_width_in_ms = float(pulse_width_str.replace("ms", ""))
+    pulse_width_in_s = pulse_width_in_ms / 1000
+
+    stimulation_parameters = dict(
+        power_in_W=power_in_mW / 1000,
+        duration_in_s=duration_in_ms / 1000,
+        frequency_in_Hz=frequency_in_Hz,
+        pulse_width_in_s=pulse_width_in_s,
+    )
+
+    return stimulation_parameters
 
 
 def add_optogenetic_stimulus_site(nwbfile: NWBFile, metadata: dict, ogen_site_name: str):
